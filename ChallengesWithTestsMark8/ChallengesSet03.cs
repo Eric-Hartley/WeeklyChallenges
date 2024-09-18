53cfa0120325235a82fcaad68b2a3ff7b2394dc8
Eric Hartley <spiffydude0@gmail.com>
2024-09-17 19:40:53 -0500

    did a couple parts

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,12 +8,42 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (var val in vals)
+            {
+                if (val == false)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            List<int> odds = new List<int>();
+            int sum = 0;
+            if (numbers == null)
+            {
+                return false;
+            }
+            foreach (var val in numbers)
+            {
+                if (val % 2 != 0)
+                {
+                    odds.Add(val);
+                }
+
+            }
+            foreach (var oddNum in odds)
+            {
+                sum += oddNum;
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
